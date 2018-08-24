f7d35c8e9deb22cfd1195177e68d63f64fce15a3
green <san5201314@gmail.com>
2018-08-24 16:35:26 +0800

    green 20180824 1635

--- mysite/lib/views.py
+++ mysite/lib/views.py
@@ -1,7 +1,12 @@
 # lib/view.py
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book
 def index(request):
     return HttpResponse("Hello, world")
+
+def detail(request):
+    book_list = Book.object.order_by('-pub_date')[:5]
+    context = {'book_list': book_list}
+    return render(request, 'lib/detail.html',context)
 # Create your views here.
