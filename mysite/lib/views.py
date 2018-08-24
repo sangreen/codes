2f0baa67fdf6156e7d7278f937a34e5f15c8d861
green <san5201314@gmail.com>
2018-08-24 17:46:55 +0800

    green 20180824 1746

--- mysite/lib/views.py
+++ mysite/lib/views.py
@@ -6,7 +6,7 @@ def index(request):
     return HttpResponse("Hello, world")
 
 def detail(request):
-    book_list = Book.object.order_by('-pub_date')[:5]
+    book_list = Book.objects.order_by('-pub_date')[:5]
     context = {'book_list': book_list}
     return render(request, 'lib/detail.html',context)
 # Create your views here.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,6 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates/').replace('\\', '/')],
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
