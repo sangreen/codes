6e073d9211953d5d52a5c636a0e723ff682f9951
green <san5201314@gmail.com>
2018-08-28 17:43:24 +0800

    green 20180828 1745

--- mysite/lib/views.py
+++ mysite/lib/views.py
@@ -1,6 +1,8 @@
 # lib/view.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Book
 def index(request):
     return HttpResponse("Hello, world")
@@ -9,4 +11,16 @@ def detail(request):
     book_list = Book.objects.order_by('-pub_date')[:5]
     context = {'book_list': book_list}
     return render(request, 'lib/detail.html',context)
+
+def addBook(request):
+	if request.method == 'POST':
+		temp_name = request.POST['name']
+		temp_author = request.POST['author']
+		temp_pub_house = request.POST['pub_house']
+
+	from django.utils import timezone
+	temp_book = Book(name=temp_name,author=temp_author,pub_house=temp_pub_house, pub_date=timezone.now())
+	temp_book.save()
+
+	return HttpResponseRedirect(reverse('lib:detail'))
 # Create your views here.
